688dcc213ed7484cd472eafd49be34f2cbacc62a
Harry Gao <harry.gao@planetart.com>
2022-02-07 18:13:41 +0800

    use modern neovim config

---
+++ lua/plugins.lua
@@ -0,0 +1,54 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  use {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+  }
+
+  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+
+  -- use "lukas-reineke/indent-blankline.nvim"
+
+  use "machakann/vim-sandwich"
+
+  use "Raimondi/delimitMate"
+
+  use 'dstein64/vim-startuptime'
+
+  use {'kevinhwang91/nvim-hlslens'}
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  use 'neovim/nvim-lspconfig'
+
+  -- use({ "Yggdroot/LeaderF",  run = ":LeaderfInstallCExtension" })
+
+  use({"tpope/vim-commentary", event = "VimEnter"})
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+end)
+
